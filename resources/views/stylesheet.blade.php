92fc8544e3708da2518fdb1a052b9c0efd408d5f
Edwin Tsatsu <tselormey99@gmail.com>
2017-12-10 12:35:12 +0000

    stylesheet asset link created

--- resources/views/stylesheet.blade.php
+++ resources/views/stylesheet.blade.php
@@ -1,26 +1,26 @@
 {{-- @if (Request::secure()) --}}
   <link rel="shortcut icon" href="{{URL::to('/')}}/favico.png" type="image/png">
 <!--right slidebar-->
-  <link href="{{ secure_asset('/css/slidebars.css') }}" rel="stylesheet">
+  <link href="{{URL::to('/')}}/css/slidebars.css" rel="stylesheet">
 
   <!--switchery-->
-  <link href="{{ secure_asset('/js/switchery/switchery.min.css')}}" rel="stylesheet" type="text/css" media="screen" />
+  <link href="{{URL::to('/')}}/js/switchery/switchery.min.css" rel="stylesheet" type="text/css" media="screen" />
 
   <!--Form Wizard-->
-  <link rel="stylesheet" type="text/css" href="{{ secure_asset('/css/jquery.steps.css') }}" />
+  <link rel="stylesheet" type="text/css" href="{{URL::to('/')}}/css/jquery.steps.css" />
 
   <!-- datatables css -->
-  <link href="{{ secure_asset('/css/jquery.dataTables.min.css') }}" rel="stylesheet">
+  <link href="{{URL::to('/')}}/css/jquery.dataTables.min.css" rel="stylesheet">
   <link rel="stylesheet" href="https://cdn.datatables.net/select/1.2.2/css/select.dataTables.min.css" rel="stylesheet">
 
   <!--common style-->
   <link href="{{URL::to('/')}}/css/style.css" rel="stylesheet">
-  <link href="{{ secure_asset('/css/style-responsive.css') }}" rel="stylesheet">
+  <link href="{{URL::to('/')}}/css/style-responsive.css" rel="stylesheet">
   <link rel="stylesheet" type="text/css" href="{{URL::to('/')}}/css/custom.css">
   @if( \Request::path() == 'privacy' || \Request::path() == 'datatable' || \Request::path() == 'services')
-  <link rel="stylesheet" href="{{ secure_asset('/css/helper.css') }}" media="screen" title="no title" charset="utf-8">
-  <script src="{{ secure_asset('/js/jquery-1.10.2.min.js') }}"></script>
-  <script src="{{ secure_asset('/js/bootstrap.min.js') }}"></script>
+  <link rel="stylesheet" href="{{URL::to('/')}}/css/helper.css" media="screen" title="no title" charset="utf-8">
+  <script src="{{URL::to('/')}}/js/jquery-1.10.2.min.js"></script>
+  <script src="{{URL::to('/')}}/js/bootstrap.min.js"></script>
   @endif
 {{-- @else
 
