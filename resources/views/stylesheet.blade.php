2ac9401fa6e9e1ce8d89c748e298aaba2f7930be
Edwin Tsatsu <tselormey99@gmail.com>
2017-12-10 13:06:43 +0000

    stylesheet fixed:

--- resources/views/stylesheet.blade.php
+++ resources/views/stylesheet.blade.php
@@ -1,27 +1,27 @@
 {{-- @if (Request::secure()) --}}
-  <link rel="shortcut icon" href="{{URL('/')}}/favico.png" type="image/png">
+  <link rel="shortcut icon" href="{{Request::secure(Request::path()).'/favico.png'}}" type="image/png">
 <!--right slidebar-->
 {{-- <link href="{{URL('/')}}/css/slidebars.css" rel="stylesheet"> --}}
   <link href="{{Request::secure(Request::path()).'/css/slidebars.css'}}" rel="stylesheet"/>
 
   <!--switchery-->
-  <link href="{{URL('/')}}/js/switchery/switchery.min.css" rel="stylesheet" type="text/css" media="screen" />
+  <link href="{{Request::secure(Request::path()).'/js/switchery/switchery.min.css'}}" rel="stylesheet" type="text/css" media="screen" />
 
   <!--Form Wizard-->
-  <link rel="stylesheet" type="text/css" href="{{URL('/')}}/css/jquery.steps.css" />
+  <link rel="stylesheet" type="text/css" href="{{Request::secure(Request::path()).'/css/jquery.steps.css'}}" />
 
   <!-- datatables css -->
-  <link href="{{URL('/')}}/css/jquery.dataTables.min.css" rel="stylesheet">
+  <link href="{{Request::secure(Request::path()).'/css/jquery.dataTables.min.css'}}" rel="stylesheet">
   <link rel="stylesheet" href="https://cdn.datatables.net/select/1.2.2/css/select.dataTables.min.css" rel="stylesheet">
 
   <!--common style-->
-  <link href="{{URL('/')}}/css/style.css" rel="stylesheet">
-  <link href="{{URL('/')}}/css/style-responsive.css" rel="stylesheet">
-  <link rel="stylesheet" type="text/css" href="{{URL('/')}}/css/custom.css">
+  <link href="{{Request::secure(Request::path()).'/css/style.css'}}" rel="stylesheet">
+  <link href="{{Request::secure(Request::path()).'/css/style-responsive.css'}}" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="{{Request::secure(Request::path()).'/css/custom.css'}}">
   @if( \Request::path() == 'privacy' || \Request::path() == 'datatable' || \Request::path() == 'services')
-  <link rel="stylesheet" href="{{URL('/')}}/css/helper.css" media="screen" title="no title" charset="utf-8">
-  <script src="{{URL('/')}}/js/jquery-1.10.2.min.js"></script>
-  <script src="{{URL('/')}}/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="{{Request::secure(Request::path()).'/css/helper.css'}}" media="screen" title="no title" charset="utf-8">
+  <script src="{{Request::secure(Request::path()).'/js/jquery-1.10.2.min.js'}}"></script>
+  <script src="{{Request::secure(Request::path()).'/js/bootstrap.min.js'}}"></script>
   @endif
 {{-- @else
 
