e2e65eb9d96723115d07dba9dab0d7ff05af0f72
Edwin Tsatsu <paynegreen@yahoo.com>
2018-06-22 16:09:58 +0000

    https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,7 +23,7 @@ class AppServiceProvider extends ServiceProvider
          );
         
         // Force SSL in production
-        if ($this->app->environment() == 'production') {
+        if ($this->app->environment() != 'development') {
             URL::forceScheme('https');
         }
     }
